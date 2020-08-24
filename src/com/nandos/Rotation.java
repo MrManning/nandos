149531f44e46f9c0745f1fafa0780c55156d7c25
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-24 21:39:42 +0100

    created template classes for rover

---
+++ src/com/nandos/Rotation.java
@@ -0,0 +1,4 @@
+package com.nandos;
+
+public class Rotation {
+}
---
+++ src/com/nandos/Rover.java
@@ -0,0 +1,18 @@
+package com.nandos;
+
+public class Rover {
+    public Position position;
+    public Rotation rotation;
+
+    public Rover() {
+
+    }
+
+    public void Move() {
+
+    }
+
+    public void Rotate(String direction) {
+        
+    }
+}
