ba9aa96d1ea49beb406323e548b28614c87754b6
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-24 21:36:37 +0100

    added reading from file

--- src/com/nandos/Main.java
+++ src/com/nandos/Main.java
@@ -1,8 +1,21 @@
 package com.nandos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        File filename = new File("src/input.txt");
 
+        try {
+            Scanner sc = new Scanner(filename);
+            while(sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch( FileNotFoundException e ) {
+            e.printStackTrace();
+        }
     }
 }
