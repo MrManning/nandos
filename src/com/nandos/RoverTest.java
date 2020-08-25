88ef67ff0fe65fc0fa7db25a67131ddb9f87ed60
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-25 20:40:06 +0100

    updated tests for rover rotation

--- src/com/nandos/RoverTest.java
+++ src/com/nandos/RoverTest.java
@@ -1,17 +1,22 @@
 package com.nandos;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoverTest {
+    @BeforeAll
+    static void init() {
+        Rover.upGrid = new int[5][5];
+    }
 
     @Test
     void updateRover() {
         Rover rover = new Rover(2,3, "N");
-        rover.updateRover("M");
-        rover.updateRover("R");
-        rover.updateRover("M");
+        rover.updateRover('M');
+        rover.updateRover('R');
+        rover.updateRover('M');
 
         assertEquals(4, rover.position.getY());
         assertEquals(3, rover.position.getX());
@@ -21,8 +26,8 @@ class RoverTest {
     @Test
     void invalidUpdateRover() {
         Rover rover = new Rover(4,4, "N");
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> rover.updateRover("M"));
-        assertEquals("invalid move", exception.getMessage());
+        rover.updateRover('M');
+        assertEquals(4, rover.position.getY());
     }
 
     @Test
@@ -31,15 +36,15 @@ class RoverTest {
         rover.moveRover();
         assertEquals(2, rover.position.getY());
 
-        rover.rotateRover("R");
+        rover.rotateRover('R');
         rover.moveRover();
         assertEquals(1, rover.position.getX());
 
-        rover.rotateRover("R");
+        rover.rotateRover('R');
         rover.moveRover();
         assertEquals(3, rover.position.getY());
 
-        rover.rotateRover("R");
+        rover.rotateRover('R');
         rover.moveRover();
         assertEquals(2, rover.position.getX());
     }
@@ -47,7 +52,7 @@ class RoverTest {
     @Test
     void rotateRover() {
         Rover rover = new Rover(4, 2, "W");
-        rover.rotateRover("L");
+        rover.rotateRover('L');
         assertEquals("S", rover.position.getHeading().name());
     }
 }
