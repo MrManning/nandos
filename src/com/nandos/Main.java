a6c7ac565a406b7d024b139af980be8dde20a918
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2020-08-25 20:43:18 +0100

    updated main loop

--- src/com/nandos/Main.java
+++ src/com/nandos/Main.java
@@ -8,14 +8,42 @@ public class Main {
 
     public static void main(String[] args) {
         File filename = new File("src/input.txt");
+        int line = 0;
 
         try {
             Scanner sc = new Scanner(filename);
+            String[] gridSize = sc.nextLine().split(" ");
+
+            Rover.upGrid = new int[Integer.parseInt(gridSize[0])][Integer.parseInt(gridSize[1])];
+
+            Rover rover = null;
             while(sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+
+                if(line % 2 == 0) {
+                    String[] inputLine = sc.nextLine().split(" ");
+
+                    int x = Integer.parseInt(inputLine[0]);
+                    int y = Integer.parseInt(inputLine[1]);
+                    String heading = inputLine[2];
+
+                    if(Movement.validPosition(x, y) && Rotation.validRotation(heading) ) {
+                        rover = new Rover(x, y, heading);
+                    }
+                } else {
+                    String instructions = sc.nextLine();
+                    for( int i = 0; i < instructions.length(); i++ ) {
+                        if( rover != null ) {
+                            rover.updateRover(instructions.charAt(i));
+                        }
+                    }
+                }
+
+                line++;
             }
         } catch( FileNotFoundException e ) {
-            e.printStackTrace();
+            System.out.println("Error: File not found");
+        } catch( NumberFormatException e ) {
+            System.out.println("Error: Invalid upper-right coordinates");
         }
     }
 }
--- src/com/nandos/Rover.java
+++ src/com/nandos/Rover.java
@@ -1,18 +1,71 @@
 package com.nandos;
 
 public class Rover {
-    public Position position;
-    public Rotation rotation;
+    public Movement position;
+    public static int[][] upGrid;
 
-    public Rover() {
+    public Rover(int x, int y, String heading) {
+        this.position = new Movement(x, y, heading);
+    }
+
+    public void updateRover(char command) {
+        if( Character.toUpperCase(command) == 'M' ) {
+            moveRover();
+        } else if( Character.toUpperCase(command) == 'L' ) {
+            rotateRover(command);
+        } else if( Character.toUpperCase(command) == 'R' ) {
+            rotateRover(command);
+        } else {
+            throw new IllegalArgumentException("invalid move");
+        }
+    }
+
+    public void moveRover() {
+        int x = position.getX();
+        int y = position.getY();
+
+        switch( position.getHeading().name() ) {
+            case "N":
+                if( Movement.validPosition(x, y + 1)) {
+                    Rover.upGrid[x][y] = 0;
+                    position.setY(y + 1);
+                    Rover.upGrid[x][y] = 1;
+                }
+                break;
+            case "E":
+                if( Movement.validPosition(x + 1, y) ) {
+                    Rover.upGrid[x][y] = 0;
+                    position.setX(x + 1);
+                    Rover.upGrid[x][y] = 1;
+                }
+                break;
+            case "S":
+                if( Movement.validPosition(x, y - 1) ) {
+                    Rover.upGrid[x][y] = 0;
+                    position.setY(y - 1);
+                    Rover.upGrid[x][y] = 1;
+                }
+                break;
+            case "W":
+                if( Movement.validPosition(x - 1, y) ) {
+                    Rover.upGrid[x][y] = 0;
+                    position.setX(x - 1);
+                    Rover.upGrid[x][y] = 1;
+                }
+                break;
+            default: break;
+        }
+
 
     }
 
-    public void Move() {
-
-    }
-
-    public void Rotate(String direction) {
-        
+    public void rotateRover(char direction) {
+        Rotation updatedHeading;
+        if( Character.toUpperCase(direction) == 'L' ) {
+            updatedHeading = position.getHeading().rotateLeft();
+        } else {
+            updatedHeading = position.getHeading().rotateRight();
+        }
+        position.setHeading(updatedHeading);
     }
 }
